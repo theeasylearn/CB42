293ada1c2243907908273681a695ed466bc9a72f
yash0623 <yash.gsispl@gmail.com>
2024-11-14 09:31:54 +0530

    updates

--- 21_Array.c
+++ 21_Array.c
@@ -1,13 +1,23 @@
 /*Array :
 index value start with 0
-
-
 **/
 #include<stdio.h>
 void main()
 {
-    int rollno[5]={101,102,103,104,105};
+    int rollno[5],i;
 
-    printf("Student 1 rollno is : %d",rollno[0])
+    for(i=0;i<5;i++)
+    {
+        printf("Enter Rollno of stdent %d : ",i+1); //i++ --> i=i+1
+        scanf("%d",&rollno[i]);
+    }
 
+    for(i=0;i<5;i++)
+    {
+        printf("\nStudent %d rollno is : %d",i+1,rollno[i]);
+    }
+
+    
+    
+    
 }
